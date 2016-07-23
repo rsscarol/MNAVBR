9168f9cd13b0954dd2cb94f4532c7ebf86b6a9cc
Carol Rodrigues <rsscarol@gmail.com>
2016-07-23 09:05:39 -0300

    Ajustes e teste contato

---
+++ .js/nomes.js
@@ -0,0 +1,9 @@
+/*function Redirect() {
+    window.location="funcoes/direção.html#carlasouza";
+}*/
+
+
+document.getElementById("#nome_"+id).addEventListener('click', function(e){
+     // Não siga o link!
+     window.location="funcoes/edicao.html#carlasouza";
+});
