b65eb835043e483be3c3c890a28efdbc64ea48b3
Carol Rodrigues <rsscarol@gmail.com>
2016-07-31 21:59:55 -0300

    Alimentação 9

--- .js/form.js
+++ .js/form.js
@@ -1,5 +1,5 @@
 
-var nome="#contatoNome";
+/*var nome="#contatoNome";
 var cidade="contatoCidade";
 var email="contatoEmail";
 var msg="contatoMsg";
@@ -7,4 +7,12 @@ var msg="contatoMsg";
 
 function confirmaContato ("confirma").onclick = function(){
     document.write("<h2>" + nome + "</h2>" + "<br/>" + cidade + "<br/>" + email + "<br/>" + "<br/>" + msg);
-}
+}*/
+
+
+$('#contatoForm').on('submit', function () {
+    var nome = $(this).find('input[name=nome]'), nome = nome.val();
+    var email = $(this).find('input[name=email]'), email = email.val();
+    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
+    $(this).find('input[name=Body]').attr('value', texto);
+});
