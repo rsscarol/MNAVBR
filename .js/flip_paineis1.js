75689759886072c80bd40531f6055514d6882b32
Carol Rodrigues <rsscarol@gmail.com>
2016-08-01 04:19:11 -0300

    Alimentação 10
    Término da primeira leva de inscritos e inserção do scroll nas páginas.
    Atualização dos links em todas as páginas.

--- .js/flip_paineis1.js
+++ .js/flip_paineis1.js
@@ -3,6 +3,9 @@
 function profissional(id){
 	$("#flip_"+id).css('display','none');
 	$("#painel_"+id).slideDown('fast',function(){
+		$('html, body').animate({
+				 scrollTop: $("#painel_"+id).offset().top
+		 }, 1000);
 	});
 }
 
@@ -12,15 +15,12 @@ function profissional_fechar(id){
 	});
 }
 
-
 window.onload = function() {
 	var urlId = window.location.hash.substring(1);
 	$ ("#flip_" + urlId).css ('display','none');
 	$ ("#painel_" + urlId).slideDown ('fast',function(){
+		$('html, body').animate({
+				 scrollTop: $("#painel_" + urlId).offset().top
+		 }, 1000);
 	});
 }
-
-var box = $ ("#alerta");
-$ ("#botao").on ("click", function (event) {
-	box.show ();
-});
