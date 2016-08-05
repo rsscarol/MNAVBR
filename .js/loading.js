83ea7909e28556e1eab655abbd3acd137b8de43d
Carol Rodrigues <rsscarol@gmail.com>
2016-08-05 03:56:31 -0300

    Correção Loading

--- .js/loading.js
+++ .js/loading.js
@@ -4,6 +4,8 @@ var i = setInterval(function () {
 
     // The desired code is only this:
     document.getElementById("loading").style.display = "none";
+    document.getElementById("content1").style.display = "block";
     document.getElementById("content").style.display = "block";
+
 
 }, 2000);
